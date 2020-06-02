5f3fa976e7de46a876f8fe767e832161b7672ee5
LendaDeAfonselio <afonsof367@gmail.com>
2020-06-02 19:24:46 +0100

    Bellman ford correct

--- src/algorithm/BellmanFordAlgorithm.java
+++ src/algorithm/BellmanFordAlgorithm.java
@@ -1,21 +1,20 @@
 package algorithm;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import domain.Edge;
 import domain.Graph;
 import domain.Vertex;
 
+
 public class BellmanFordAlgorithm {
-	public Map<String, Double> getShortestPath(Vertex init, Graph graph) {
+	public double[] getShortestPath(Vertex init, Graph graph) {
 		// setup
 		int graphLength = graph.vertexNumber();
 		double infinity = Double.POSITIVE_INFINITY;
 		double[] distances = new double[graphLength];
 		List<Edge> edges = graph.getEdges();
-		List<Vertex> vertexList = graph.getVertexes();
+		List<Vertex> vertexList = graph.getIndexedVertexes();
 
 		// Initialize the map with distances to each vertex being infinite - O(V)
 		int i = 0;
@@ -25,13 +24,12 @@ public class BellmanFordAlgorithm {
 			}
 		}
 
-		// Isto nao estah certo xD
 		// Percorrer todos os vertices
-		// Percorrer todas as arestas
-		// atualizar o no array das distancias sse:
-		// o valor atual - currentdistance - nao for infinito
-		// currentdistance + o peso da aresta atual sao menores do que as que estao
-		// guardadas na posicao do destino
+			// Percorrer todas as arestas
+			// atualizar o no array das distancias sse:
+				// o valor atual - currentdistance - nao for infinito
+				// currentdistance + o peso da aresta atual sao menores do que as que estao
+				// guardadas na posicao do destino
 		i = 0;
 		for (Vertex v : vertexList) {
 			if (!v.equals(init)) {
@@ -40,25 +38,13 @@ public class BellmanFordAlgorithm {
 					Vertex source = edge.getSource();
 					Vertex destiny = edge.getDestiny();
 					double currentDistance = distances[source.getPosition()];
-					//if 
+					if (currentDistance != infinity && currentDistance + weight < distances[destiny.getPosition()]) {
+						distances[destiny.getPosition()] = currentDistance + weight;
+					}
 				}
 			}
 			i++;
 		}
-		// for (Vertex v : graph.graph) {
-			// if (!v.equals(init)) {
-				// for (Map.Entry<Vertex, Double> edge : v.getOutgoingEdges().entrySet()) {
-					// double weight = edge.getValue();
-					// Vertex source = v;
-					// Vertex destiny = edge.getKey();
-					// double currentDistance = distances.get(source.getLabel());
-					// if (currentDistance != infinity && currentDistance + weight <
-						// distances.get(destiny.getLabel())) {
-						// distances.replace(destiny.getLabel(), currentDistance + weight);
-						// }
-				// }
-			// }
-		// }
 
 		return distances;
 	}
