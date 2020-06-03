cfa32101e4d901a9ffc2c8003d72f6917febd208
LendaDeAfonselio <afonsof367@gmail.com>
2020-06-03 19:28:41 +0100

    melhorias no algoritmo para performance

--- src/algorithm/BellmanFordAlgorithm.java
+++ src/algorithm/BellmanFordAlgorithm.java
@@ -6,46 +6,85 @@ import domain.Edge;
 import domain.Graph;
 import domain.Vertex;
 
-
 public class BellmanFordAlgorithm {
-	public double[] getShortestPath(Vertex init, Graph graph) {
+	public double[] getShortestPath(Vertex init, Graph graph) throws Exception {
 		// setup
+		
 		int graphLength = graph.vertexNumber();
 		double infinity = Double.POSITIVE_INFINITY;
 		double[] distances = new double[graphLength];
 		List<Edge> edges = graph.getEdges();
 		List<Vertex> vertexList = graph.getIndexedVertexes();
+		
+		// debug e testes
+		int iterated_times = 0;
 
 		// Initialize the map with distances to each vertex being infinite - O(V)
 		int i = 0;
 		for (Vertex vertex : vertexList) {
 			if (!vertex.equals(init)) {
-				distances[i++] = infinity;
-			}
-		}
-
-		// Percorrer todos os vertices
-			// Percorrer todas as arestas
-			// atualizar o no array das distancias sse:
-				// o valor atual - currentdistance - nao for infinito
-				// currentdistance + o peso da aresta atual sao menores do que as que estao
-				// guardadas na posicao do destino
-		i = 0;
-		for (Vertex v : vertexList) {
-			if (!v.equals(init)) {
-				for (Edge edge : edges) {
-					double weight = edge.getWeight();
-					Vertex source = edge.getSource();
-					Vertex destiny = edge.getDestiny();
-					double currentDistance = distances[source.getPosition()];
-					if (currentDistance != infinity && currentDistance + weight < distances[destiny.getPosition()]) {
-						distances[destiny.getPosition()] = currentDistance + weight;
-					}
-				}
+				distances[i] = infinity;
 			}
 			i++;
 		}
 
+		// Percorrer todos os vertices
+		// Percorrer todas as arestas
+		// atualizar o no array das distancias sse:
+		// o valor atual - currentdistance - nao for infinito
+		// currentdistance + o peso da aresta atual sao menores do que as que estao
+		// guardadas na posicao do destino
+
+		boolean changesFromPreviousIteration = true;
+	
+		for (Vertex v : vertexList) {
+			// a ordem eh a mesma de uma iteracao para a outra, portanto se nao houver
+			// mudancas na tabela de distancias entao nao eh necessahrio continuar a iterar
+			// uma vez que o resultado das iteracoes seguintes serah sempre o mesmo.
+			if (!changesFromPreviousIteration) {
+				break;
+			}
+			if (!v.equals(init)) { // V - 1 vezes que eh preciso iterar
+				changesFromPreviousIteration = false;
+				for (Edge edge : edges) {
+					// setup
+					double weight = edge.getWeight();
+					Vertex source = edge.getSource();
+					Vertex destiny = edge.getDestiny();
+
+					// se o noh de destino eh igual ao no onde comecamos a distancia sera 0 e na
+					// realidade nao interssa para este problema, portanto poupamos assim trabalho
+					if (!destiny.equals(init)) {
+						double currentDistance = distances[source.getPosition()];
+						if (currentDistance != infinity
+								&& currentDistance + weight < distances[destiny.getPosition()]) {
+							distances[destiny.getPosition()] = currentDistance + weight;
+							changesFromPreviousIteration = true;
+						}
+					}
+					iterated_times++;
+				}
+			}
+			
+			// iterated_times++;
+		}
+		System.out.println(iterated_times);
+
+		// Verificar ciclos infinitos que permitem decrescer o valor para "sempre".
+		for (Edge e : edges) {
+			Vertex origin = e.getSource();
+			Vertex dest = e.getDestiny();
+
+			double weight = e.getWeight();
+			double currentDistance = distances[origin.getPosition()];
+
+			// Se houver um caminho "ainda" mais curto entao ha um ciclo negativo que eh
+			// infinito
+			// uma vez que reduz a cada passagem
+			if (currentDistance != infinity && currentDistance + weight < distances[dest.getPosition()]) {
+				throw new Exception("Infinite path discovered");
+			}
+		}
 		return distances;
 	}
 
---
+++ src/examples/ExemploNormal.java
@@ -0,0 +1,53 @@
+package examples;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import algorithm.BellmanFordAlgorithm;
+import domain.Edge;
+import domain.Graph;
+import domain.Vertex;
+
+public class ExemploNormal {
+
+	public static void main(String[] args) throws Exception {
+		BellmanFordAlgorithm bfa = new BellmanFordAlgorithm();
+
+		// cycle loop example - Should throw exception
+		Vertex a = new Vertex("a");
+		Vertex b = new Vertex("b");
+		Vertex c = new Vertex("c");
+		Vertex d = new Vertex("d");
+
+		Edge ac = new Edge(a, c, 0);
+		Edge db = new Edge(d, b, -300.0);
+		Edge ad = new Edge(a, d, 99.0);
+		Edge ab = new Edge(a, b, 1.0);
+		Edge bc = new Edge(b, c, 1.0);
+
+		ArrayList<Vertex> verts = new ArrayList<>();
+		verts.add(a);
+		verts.add(b);
+		verts.add(c);
+		verts.add(d);
+
+		ArrayList<Edge> edges = new ArrayList<>();
+		edges.add(ac);
+		edges.add(ad);
+		edges.add(ab);
+		edges.add(db);
+		edges.add(bc);
+
+		Graph graph = new Graph(verts, edges);
+
+		double[] result = bfa.getShortestPath(a, graph);
+		int i = 0;
+		for (Vertex v : verts) {
+			if (!v.equals(a)) {
+				System.out.println(String.format(Locale.ENGLISH, "Shortest path to object %s has cost %.3f", v.getLabel(), result[i]));
+			}
+			i++;
+		}
+
+	}
+
+}
